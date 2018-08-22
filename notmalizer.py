1d3a15273bd27317901fba2a407b34564f5d2bcf
Peter Beattie <peterbeattie19@gmail.com>
2018-08-22 20:00:52 +0100

    taking means and STDs

--- notmalizer.py
+++ notmalizer.py
@@ -3,8 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = np.random.randint(100, size = 100)
+print(tmean(data))
 std = tstd(data)
+print(std)
+plt.plot(data)
+plt.show()
+
 data = list(map(lambda x: x/std, data)) 
+
+print(tmean(data))
+print(tstd(data)) 
 
 plt.plot(data)
 plt.show()
